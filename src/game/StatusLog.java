491b9ec2e391fbcc338ddc8ed2037f6add6dadc9
t22cs044 <t22cs044@yamanashi.ac.jp>
2024-05-31 19:20:54 +0900

    Scoreの追加

--- src/game/StatusLog.java
+++ src/game/StatusLog.java
@@ -9,6 +9,7 @@ public class StatusLog {
     StringService title;
     StringService time;
     StringService hp;
+    StringService score;
 
     /**
      * ステータスログのコンストラクター
@@ -25,6 +26,7 @@ public class StatusLog {
         title = new StringService(manager.master.view, "title", 0, 0, CColor.BLACK, CColor.WHITE, true);
         time = new StringService(manager.master.view, "00:00", 0, 0, CColor.BLACK, CColor.WHITE, true);
         hp = new StringService(manager.master.view, "⭐⭐⭐", 0, 0, CColor.BLACK, CColor.WHITE, true);
+        score = new StringService(manager.master.view, "SCORE:", 0, 0, CColor.BLACK, CColor.WHITE, true);
         popup.SetLocation(
                 manager.master.view.getCameraX() + manager.master.view.getWidth()
                         - popup.GetCostumeData().get(0).size()
@@ -45,22 +47,25 @@ public class StatusLog {
                         - popup.GetCostumeData().get(0).size()
                         - 1,
                 manager.master.view.getCameraY() + 1);
-        if (Math.pow(manager.player.X-popup.getX(),2)+Math.pow(manager.player.Y-popup.getY(),2)<=400){
+        if (Math.pow(manager.player.X-popup.getX(),2)+Math.pow(manager.player.Y-popup.getY(),2)<=625){
             popup.SetLocation(manager.master.view.getCameraX()+1, manager.master.view.getCameraY()+1);
         }
 
         title.SetLocation(popup.X + 2, popup.Y + 2);
         time.SetLocation(popup.X + 2, popup.Y + 3);
         hp.SetLocation(popup.X + 2, popup.Y + 4);
+        score.SetLocation(popup.X + 2, popup.Y + 5);
         time.ChangeString("TIME:" + _time);
         String strtmp = "";
         for (int i = 0; i < _hp; i++) {
             strtmp += "⭐";
         }
         hp.ChangeString("HP:" + strtmp);
+        score.ChangeString("SCORE:" + manager.score);
         popup.ChangeDrawingOrder(-1);
         title.ChangeDrawingOrder(-1);
         time.ChangeDrawingOrder(-1);
+        score.ChangeDrawingOrder(-1);
         hp.ChangeDrawingOrder(-1);
 
     }
