b3c699fbfc505b7e4394b264ecb1ec69de492838
t22cs044 <t22cs044@yamanashi.ac.jp>
2024-06-09 02:40:52 +0900

    一連のゲームの流れを実装

--- src/master/Game.java
+++ src/master/Game.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import consolegui.*;
 import game.*;
 import result.*;
+import start.*;
 
 public class Game {
 
@@ -24,11 +25,21 @@ public class Game {
      * アプリケーションの実行
      */
     public void run() throws InterruptedException, IOException {
-        // GameScene master = new GameScene(this);
-        // Result gameresult = master.run();
-        Result gameresult = new Result(true, 100,15,100);
-        ResultScene result = new ResultScene(this, gameresult);
-        result.run();
+        while(true){
+            MainMenuController mainmenu = new MainMenuController(this);
+            boolean wantToExit = mainmenu.run();
+            if (wantToExit) {
+                break;
+            }
+            GameScene master = new GameScene(this);
+            Result gameresult = master.run();
+            // Result gameresult = new Result(true, 100,15,100);
+            ResultScene result = new ResultScene(this, gameresult);
+            result.run();
+        }
+        ExitDisplay exit = new ExitDisplay(this);
+        exit.run();
+        key.RemoveMe();
 
     }
 }
