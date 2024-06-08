90c81c1b501aa161ebb8f7a8ef366f30f5946ac2
t22cs044 <t22cs044@yamanashi.ac.jp>
2024-06-09 02:42:24 +0900

    遊び方のファイルを置いた

--- src/start/HowToPlayScene.java
+++ src/start/HowToPlayScene.java
@@ -1,5 +1,30 @@
 package start;
 
+import consolegui.*;
+
 public class HowToPlayScene {
-    
+    private class parts {
+        public CObject title;
+        public CObject playButton;
+        public CObject rankingButton;
+        public CObject howToPlayButton;
+        public CObject exitButton;
+    }
+
+    public MainMenuController manager;
+    public parts parts;
+
+    public HowToPlayScene(MainMenuController manager){
+        this.manager = manager;
+        this.parts = new parts();
+    }
+
+    public void run(){
+
+    }
+
+    public void destroy(){
+        
+    }
+
 }
---
+++ src/start/howtoplay/Page1.java
@@ -0,0 +1,5 @@
+package start.howtoplay;
+
+public class Page1 {
+    
+}
