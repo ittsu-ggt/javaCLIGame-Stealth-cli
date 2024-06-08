68c5b4187b38107fb8a5a2239198dea4d6ee1c4c
t22cs044 <t22cs044@yamanashi.ac.jp>
2024-06-09 02:41:24 +0900

    メインメニューの大部分を実装

--- src/start/MainMenuController.java
+++ src/start/MainMenuController.java
@@ -1,14 +1,66 @@
 package start;
 
-import consolegui.CObject;
+import consolegui.*;
 import master.*;
 
 public class MainMenuController {
-    private Game master;
+    private class defaultcolor{
+        public int backGroundColor;
+        public int wordColor;
+    }
+    Game master;
     
     private MainScene mainScene;
     private RankingScene rankingScene;
+    private defaultcolor defaultcolor;
     private HowToPlayScene howToPlayScene;
+
+    public MainMenuController(Game master) {
+        this.master = master;
+        this.defaultcolor = new defaultcolor();
+        this.defaultcolor.backGroundColor = master.view.defaultBackGroundColor;
+        this.defaultcolor.wordColor = master.view.defaultWordColor;
+    }
+
+    public boolean run(){
+        master.view.defaultBackGroundColor = CColor.WHITE;
+        master.view.defaultWordColor = CColor.WHITE;
+        boolean isEnd = false;
+        while(true){
+            this.mainScene = new MainScene(this);
+            int select = mainScene.run();
+            mainScene = null;
+            if(select == 0){
+                isEnd = false;
+                break;
+            }
+            else if(select == 1){
+                this.rankingScene = new RankingScene(this);
+                rankingScene.run();
+                rankingScene = null;
+            }
+            else if(select == 2){
+                this.howToPlayScene = new HowToPlayScene(this);
+                howToPlayScene.run();
+                howToPlayScene = null;
+            }
+            else if(select == 3){
+                isEnd = true;
+                break;
+            }else{
+                throw new RuntimeException("MainMenuController : 不正な操作です．Select = " + select);
+            }
+        }
+        master.view.defaultBackGroundColor = defaultcolor.backGroundColor;
+        master.view.defaultWordColor = defaultcolor.wordColor;
+        return isEnd;        
+    }
+
+    public void destroy(){
+        mainScene = null;
+        rankingScene = null;
+        howToPlayScene = null;
+    }
 
 
 }
