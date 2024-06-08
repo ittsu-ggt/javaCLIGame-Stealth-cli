527836b5ae8b41f9d0f2636b0576d00636f9aa9f
t22cs044 <t22cs044@yamanashi.ac.jp>
2024-06-08 23:52:21 +0900

    start画面関連のシーン追加

---
+++ src/start/MainMenuController.java
@@ -0,0 +1,14 @@
+package start;
+
+import consolegui.CObject;
+import master.*;
+
+public class MainMenuController {
+    private Game master;
+    
+    private MainScene mainScene;
+    private RankingScene rankingScene;
+    private HowToPlayScene howToPlayScene;
+
+
+}
--- src/start/RankingScene.java
+++ src/start/RankingScene.java
@@ -1,3 +1,5 @@
+package start;
+
 public class RankingScene {
     
 }
