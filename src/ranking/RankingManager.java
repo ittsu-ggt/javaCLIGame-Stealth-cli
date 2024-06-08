686e7c88dcfbbd8eb67eeb60a4577d9205db7876
t22cs044 <t22cs044@yamanashi.ac.jp>
2024-06-08 23:51:19 +0900

    ランキングManagerの本実装

--- src/ranking/RankingManager.java
+++ src/ranking/RankingManager.java
@@ -1,39 +1,70 @@
 package ranking;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class RankingManager {
     private static final String FILE_NAME = "./data/ranking/ranking.txt";
 
-    public static ArrayList<Integer> DataRoad() {
-        List<String> lines = FileReader(FILE_NAME);
-        ArrayList<Integer> data = new ArrayList<Integer>();
-        for (String line : lines) {
-            data.add(Integer.parseInt(line));
+    public static ArrayList<RankingCell> DataRoad(String FilePath) {
+        Path File_Path = Path.of(FilePath);
+        try{
+            List<String> lines = Files.readAllLines(File_Path, StandardCharsets.UTF_8);
+            ArrayList<RankingCell> data = new ArrayList<RankingCell>();
+            for (String line : lines) {
+                String[] tmp = line.split(",");
+                int score = Integer.parseInt(tmp[0]);
+                long time = Long.parseLong(tmp[1]);
+                data.add(new RankingCell(score, time));
+            }
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Path tmp = File_Path.toAbsolutePath();
+            throw new RuntimeException(
+                    "RankingManager : ファイルの読み込みに失敗しました．ファイル名 : " + tmp.toString());
         }
-        return DataSort(data);
+    }
+    public static boolean DataSave(RankingCell data) {
+        return DataSave(data, FILE_NAME);
+    }
+    public static ArrayList<RankingCell> DataRoad() {
+        return DataRoad(FILE_NAME);
     }
 
-    public static boolean DataSave(int data) {
-        List<Integer> lines = DataRoad();
-        Collections.sort(lines);
+    public static boolean DataSave(RankingCell data, String FilePath) {
+        ArrayList<RankingCell> lines = DataRoad(FilePath);
+        Collections.sort(lines, Collections.reverseOrder());
+        boolean RewriteRecord = false;
         for(int i=0;i<5;i++){
-            if(lines.get(i)<=data){
-                return true;
+            if(lines.get(i).compareTo( data)>=0){
+                RewriteRecord = true;
             }
         }
         lines.add(data);
+        Collections.sort(lines, Collections.reverseOrder());
         // 上から５つを残す
         if (lines.size() > 5) {
             lines.remove(5);
         }
-        Collections.sort(lines);
-        FileWriter(FILE_NAME, lines);
-        return;
+        Collections.sort(lines, Collections.reverseOrder());
+        List<String> writeData = new ArrayList<String>();
+        for (RankingCell line : lines) {
+            writeData.add(line.toString());
+        }
+        FileWriter(FilePath, writeData);
+        return RewriteRecord;
     }
 
-    private static ArrayList<Integer> DataSort(Array<Integer> data) {
+    private static ArrayList<RankingCell> DataSort(ArrayList<RankingCell> data) {
         Collections.sort(data);
+        return data;
     }
 
     private static List<String> FileReader(String FilePath) {
@@ -45,8 +76,8 @@ public class RankingManager {
         } catch (IOException e) {
             e.printStackTrace();
             Path tmp = File_Path.toAbsolutePath();
-            throw new IllegalArgumentException(
-                    SpriteBuildService.class.getName() + " : ファイルの読み込みに失敗しました．ファイル名 : " + tmp.toString());
+            throw new RuntimeException(
+                    "RankingManager : ファイルの読み込みに失敗しました．ファイル名 : " + tmp.toString());
         }
     }
 
@@ -57,8 +88,8 @@ public class RankingManager {
         } catch (IOException e) {
             e.printStackTrace();
             Path tmp = File_Path.toAbsolutePath();
-            throw new IllegalArgumentException(
-                    SpriteBuildService.class.getName() + " : ファイルの書き込みに失敗しました．ファイル名 : " + tmp.toString());
+            throw new RuntimeException(
+                    "RankingManager : ファイルの書き込みに失敗しました．ファイル名 : " + tmp.toString());
         }
     }
 
