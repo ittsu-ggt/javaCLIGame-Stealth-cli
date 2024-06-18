10e2083b8f3c23abc00f1b1b2dc1caa8a59014d7
t22cs044 <t22cs044@yamanashi.ac.jp>
2024-06-19 01:34:37 +0900

    不要なパスを削除

--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,6 @@
 import master.*;
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class Model {
     public static void main(String[] args) throws InterruptedException, IOException {
