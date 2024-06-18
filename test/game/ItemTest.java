390e1766c00fba3ac1a8b605800596e25dda1d6a
t22cs044 <t22cs044@yamanashi.ac.jp>
2024-06-19 01:37:49 +0900

    テストの追加

---
+++ test/game/ItemTest.java
@@ -0,0 +1,12 @@
+package game;
+
+import master.Game;
+
+public class ItemTest {
+    public Game Testmaster = new Game();
+
+    @Test
+    public void Itemの生成テスト()throws Exception{
+        Item map = new Item(new GameScene(Testmaster),0,0,true);
+    }
+}
